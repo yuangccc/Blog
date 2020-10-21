6ee284a8ad4c66350e9dff5930604e251723e2cb
yuangccc <937714759@qq.com>
2020-10-21 23:25:32 +0800

    second commit

---
+++ src/main/java/com/yuangcc/web/IndexController.java
@@ -0,0 +1,24 @@
+package com.yuangcc.web;
+
+
+import com.yuangcc.NotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name) {
+//        int i = 1/0;
+//        String blog = null;
+//        if(blog == null){
+//            throw new NotFoundException("博客不存在");
+//        }
+
+        System.out.println("-------index---------");
+
+        return "index";
+    }
+}
